7a8ee86df941a7c480119bebae08e85fdbbbeb6a
zosialenarczyk <zosia.lenarczyk1110@gmail.com>
2026-06-07 14:48:30 +0200

    functions comments

--- ingestion/utils.py
+++ ingestion/utils.py
@@ -10,6 +10,9 @@ import os
 import datetime
 
 def get_db_credentials() -> list[str]:
+    '''
+    Loads the .env file and returns the list of database connection credentials.
+    '''
     load_dotenv()
     host = os.getenv("DB_host")
     user = os.getenv("DB_user")
@@ -19,12 +22,17 @@ def get_db_credentials() -> list[str]:
 
     return [user, password, db, host, port]
 
-def connect_to_db(user, password, host, db_name) -> sqlalchemy.engine.base.Engine:
+def connect_to_db(user: str, password: str, host: str, db_name: str) -> sqlalchemy.engine.base.Engine:
+    """
+    Creates sqlalchemy engine to connect to Postgres database. 
+    """
     engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}/{db_name}")
     return engine
 
 def request_crash_data(endpoint_url: str, rows: int, last_extract_date: str) -> requests.models.Response:
-
+    '''
+    Performs an API call requesting crash data. Returns the API response.
+    '''
     params = {
         "$limit" : rows,
         "$where" : f"crash_date > '{last_extract_date}'"
@@ -36,6 +44,11 @@ def request_crash_data(endpoint_url: str, rows: int, last_extract_date: str) -> 
 
 
 def create_df_to_load(response: requests.models.Response) -> pd.DataFrame:
+        '''
+        Converts the API reponse into pandas Dataframe. 
+        Creates a variable 'download_datetime' with the download datetime stamp for all rows.
+        Returns the dataframe.
+        '''
         data = StringIO(response.text)
         df = pd.read_csv(data)
 
@@ -46,6 +59,9 @@ def create_df_to_load(response: requests.models.Response) -> pd.DataFrame:
 
 
 def load_to_db(table_name: str, df: pd.DataFrame, engine: sqlalchemy.engine.base.Engine) -> None:
+    '''
+    Loads the data in the dataframe to the database by appending to already existing rows - incremental load.
+    '''
     df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
 
 
